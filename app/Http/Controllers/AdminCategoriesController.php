390025b3240a5a9bc627fa0da88a926aab50a6c6
Luis Casotti <casotti@gmail.com>
2015-12-25 21:46:02 -0200

    Adding CRUD routes and methods

--- app/Http/Controllers/AdminCategoriesController.php
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -15,13 +15,78 @@ class AdminCategoriesController extends Controller
 	 */
 	private $category;
 
-	public function __construct(Category $category)
-	{
+	public function __construct(Category $category) {
 		$this->category = $category;
 	}
 
 	public function index() {
 		$categories = $this->category->all();
 		return view('admin.categories', compact('categories'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,5 +16,24 @@ Route::get('/', 'WelcomeController@index');
 /**
  * Admin Routes
  */
-Route::get( '/admin/categories', 'AdminCategoriesController@index' );
-Route::get( '/admin/products', 'AdminProductsController@index' );
+Route::group(['prefix' => 'admin'], function() {
+    Route::group(['prefix' => 'categories'], function(){
+        Route::get( '/', ['as'=>'admin.categories', 'uses' => 'AdminCategoriesController@index'] );
+        Route::get( '/create', ['as'=>'admin.categories.create', 'uses' => 'AdminCategoriesController@create'] );
+        Route::post( '/store', ['as'=>'admin.categories.store', 'uses' => 'AdminCategoriesController@store'] );
+        Route::get( '/show', ['as'=>'admin.categories.show', 'uses' => 'AdminCategoriesController@show'] );
+        Route::get( '/edit', ['as'=>'admin.categories.edit', 'uses' => 'AdminCategoriesController@edit'] );
+        Route::put( '/update', ['as'=>'admin.categories.update', 'uses' => 'AdminCategoriesController@update'] );
+        Route::delete( '/destroy', ['as'=>'admin.categories.destroy', 'uses' => 'AdminCategoriesController@destroy'] );
+    });
+
+    Route::group(['prefix' => 'products'], function(){
+        Route::get( '/', ['as'=> 'admin.products', 'uses' => 'AdminProductsController@index'] );
+        Route::get( '/create', ['as'=>'admin.products.create', 'uses' => 'AdminProductsController@create'] );
+        Route::post( '/store', ['as'=>'admin.products.store', 'uses' => 'AdminProductsController@store'] );
+        Route::get( '/show', ['as'=>'admin.products.show', 'uses' => 'AdminProductsController@show'] );
+        Route::get( '/edit', ['as'=>'admin.products.edit', 'uses' => 'AdminProductsController@edit'] );
+        Route::put( '/update', ['as'=>'admin.products.update', 'uses' => 'AdminProductsController@update'] );
+        Route::delete( '/destroy', ['as'=>'admin.products.destroy', 'uses' => 'AdminProductsController@destroy'] );
+    });
+});
