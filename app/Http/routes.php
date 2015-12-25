159396ab854acce86a8e55b4bb137b6e6a460a3b
Luis Casotti <casotti@gmail.com>
2015-12-25 21:52:15 -0200

    Adding Route parameters missing

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,19 +21,19 @@ Route::group(['prefix' => 'admin'], function() {
         Route::get( '/', ['as'=>'admin.categories', 'uses' => 'AdminCategoriesController@index'] );
         Route::get( '/create', ['as'=>'admin.categories.create', 'uses' => 'AdminCategoriesController@create'] );
         Route::post( '/store', ['as'=>'admin.categories.store', 'uses' => 'AdminCategoriesController@store'] );
-        Route::get( '/show', ['as'=>'admin.categories.show', 'uses' => 'AdminCategoriesController@show'] );
-        Route::get( '/edit', ['as'=>'admin.categories.edit', 'uses' => 'AdminCategoriesController@edit'] );
-        Route::put( '/update', ['as'=>'admin.categories.update', 'uses' => 'AdminCategoriesController@update'] );
-        Route::delete( '/destroy', ['as'=>'admin.categories.destroy', 'uses' => 'AdminCategoriesController@destroy'] );
+        Route::get( '/show/{id}', ['as'=>'admin.categories.show', 'uses' => 'AdminCategoriesController@show'] );
+        Route::get( '/edit/{id}', ['as'=>'admin.categories.edit', 'uses' => 'AdminCategoriesController@edit'] );
+        Route::put( '/update/{id}', ['as'=>'admin.categories.update', 'uses' => 'AdminCategoriesController@update'] );
+        Route::delete( '/destroy/{id}', ['as'=>'admin.categories.destroy', 'uses' => 'AdminCategoriesController@destroy'] );
     });
 
     Route::group(['prefix' => 'products'], function(){
         Route::get( '/', ['as'=> 'admin.products', 'uses' => 'AdminProductsController@index'] );
         Route::get( '/create', ['as'=>'admin.products.create', 'uses' => 'AdminProductsController@create'] );
         Route::post( '/store', ['as'=>'admin.products.store', 'uses' => 'AdminProductsController@store'] );
-        Route::get( '/show', ['as'=>'admin.products.show', 'uses' => 'AdminProductsController@show'] );
-        Route::get( '/edit', ['as'=>'admin.products.edit', 'uses' => 'AdminProductsController@edit'] );
-        Route::put( '/update', ['as'=>'admin.products.update', 'uses' => 'AdminProductsController@update'] );
-        Route::delete( '/destroy', ['as'=>'admin.products.destroy', 'uses' => 'AdminProductsController@destroy'] );
+        Route::get( '/show/{id}', ['as'=>'admin.products.show', 'uses' => 'AdminProductsController@show'] );
+        Route::get( '/edit/{id}', ['as'=>'admin.products.edit', 'uses' => 'AdminProductsController@edit'] );
+        Route::put( '/update/{id}', ['as'=>'admin.products.update', 'uses' => 'AdminProductsController@update'] );
+        Route::delete( '/destroy/{id}', ['as'=>'admin.products.destroy', 'uses' => 'AdminProductsController@destroy'] );
     });
 });
