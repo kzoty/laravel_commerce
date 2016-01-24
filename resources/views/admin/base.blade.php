daa7cb5376ce4c86c9004ff9542e3110527cb7db
Luis Casotti <casotti@gmail.com>
2016-01-24 18:03:19 -0200

    Fixing nav bar and links

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -9,7 +9,11 @@
 </head>
 <body>
 <nav class="navbar navbar-default">
-
+    <div class="navbar-header">
+        <div class="navbar-brand">ADMIN</div>
+    </div>
+    <div class="navbar-text"><a href="{{route('admin.categories')}}">Categories</a></div>
+    <div class="navbar-text"><a href="{{route('admin.products')}}">Products</a></div>
 </nav>
 <div class="container">
     @yield('content')
