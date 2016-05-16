697aebc3226792d3af2e423afd9d12524354db8a
Luis Casotti <luis.casotti@inphonex.com>
2016-05-16 09:50:28 -0300

    Adding rule to show or not image for Featured

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -2,6 +2,8 @@
 
 namespace CodeCommerce\Http\Controllers;
 
+use CodeCommerce\Category;
+use CodeCommerce\Product;
 use Illuminate\Http\Request;
 
 use CodeCommerce\Http\Requests;
@@ -9,8 +11,10 @@ use CodeCommerce\Http\Controllers\Controller;
 
 class StoreController extends Controller {
 
-
     public function index() {
-        return view( 'store.index' );
+        $categories = Category::all();
+        $prodsFeatured = Product::where("featured","=", 1)->get();
+        
+        return view( 'store.index', compact('categories', 'prodsFeatured') );
     }
 }
